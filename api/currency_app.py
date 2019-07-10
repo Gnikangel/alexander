b9e9e4f223f459e0406fe1d56ebb5d11777f81db
gnikangel <jvillegas@ecaresoft.com.mx>
2019-07-10 16:51:25 -0500

    Feature[Logica] Se creo la logica funcional en la distintas clases

---
+++ api/currency_app.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from pprint import pprint
+
+from lxml import etree
+from requests import get
+from models.currency_rate import CurrencyRate
+from app import db
+
+
+def fetch_app_data(fil, isoTo):
+    rate = 1.0
+    for rate_entry in list(fil):
+        currency_code = ''
+        if(isoTo == rate_entry.find('.//a').text):
+            currency_code = rate_entry.find('.//a').text
+            rate = float(rate_entry.find(
+                "td[@class='historicalRateTable-rateHeader']").text)
+    return round(rate, 6)
+
+
+def fetch_web_currency_rate(isofrom, isoTo, date):
+    rateRound = 1.0
+    url_format = (
+        'http://www.xe.com/currencytables/?'
+        'from=%(currency_code)s&date=%(date)s')
+
+    try:
+        fetched_data = get(url_format % {
+            'currency_code': isofrom, 'date': date})
+    except Exception:
+        print("Log -> ERROR al obtener las converciones")
+
+    if(fetched_data):
+        htmlelem = etree.fromstring(fetched_data.content, etree.HTMLParser())
+        rates_table = list(htmlelem.find(
+            ".//table[@id='historicalRateTbl']/tbody"))
+        filtew = filter(lambda tr: type(tr) != etree._Comment, rates_table)
+        rateRound = fetch_app_data(filtew, isoTo)
+    else:
+        pprint('Log -> Currency rate: %f' % rateRound)
+
+    return rateRound
--- models/currency_rate.py
+++ models/currency_rate.py
@@ -1,4 +1,4 @@
-
+from pprint import pprint
 from app import db
 
 
@@ -6,13 +6,14 @@ class CurrencyRate(db.Model):
     __tablename__ = 'currency_rate'
 
     currency_rate_id = db.Column(db.Integer, primary_key=True)
-    iso_code = db.Column(db.String(3), nullable=False)
+    iso_code_to = db.Column(db.String(3), nullable=False)
+    iso_code_from = db.Column(db.String(3), nullable=False)
     date = db.Column(db.DateTime(), nullable=False)
     rate = db.Column(db.Float(), nullable=False)
 
-    def __init__(self, currency_rate_id, iso_code, date, rate):
-        self.currency_rate_id = currency_rate_id
-        self.iso_code = iso_code
+    def __init__(self, iso_code_to, iso_code_from, date, rate):
+        self.iso_code_to = iso_code_to
+        self.iso_code_from = iso_code_from
         self.date = date
         self.rate = rate
 
@@ -22,7 +23,16 @@ class CurrencyRate(db.Model):
     def serialize(self):
         return {
             'currency_rate_id': self.currency_rate_id,
-            'iso_code': self.iso_code,
+            'iso_code_to': self.iso_code_to,
+            'iso_code_from': self.iso_code_from,
             'date': self.date,
             'rate': self.rate,
         }
+
+# Inserta el nuevo registro de conversion a la Base de datos
+    def insert_new_convertion_rate(isoTo, isoFrom, rate, date):
+        objToInsert = CurrencyRate(isoTo, isoFrom, date, rate)
+        db.session.add(objToInsert)
+        db.session.commit()
+        pprint(objToInsert)
+        return objToInsert
